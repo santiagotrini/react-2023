525073f19c616f1d518f595a497e0fecff99033a
Santiago Trini <santiagotrini@gmail.com>
2023-08-28 17:49:07 -0300

    agrega ejercicio 29

--- src/29-trivia/Trivia.jsx
+++ src/29-trivia/Trivia.jsx
@@ -1,9 +1,77 @@
+import { useState } from 'react';
+import questions from './questions';
+
 const Trivia = props => {
+
+  const [playing, setPlaying] = useState(false); 
+  const [gameOver, setGameOver] = useState(false); 
+  const [count, setCount] = useState(0);
+  const restartGame = () => {
+   setPlaying(false);
+   setGameOver(false);
+   setCount(0);
+  };
+
+  if (!playing && !gameOver)
+    return <Start setPlaying={setPlaying} />; 
+  else if (gameOver)
+    return <GameOver count={count} restartGame={restartGame} />;
+  else
+    return <Game count={count} setCount={setCount} questions={questions} setGameOver={setGameOver} />;
+
+};
+
+const Game = props => {
+  const [idx, setIdx] = useState(0);
+  
+  const handleClick = e => {
+    if (e.target.textContent === questions[idx].correct) {
+      setCount(count+1);
+      e.target.classList.add('green');
+    } else {
+      e.target.classList.add('red');
+    } 
+    setTimeout(() => {
+      if (idx+1 === questions.length) 
+        setGameOver(true)
+      else {
+        e.target.classList.remove('red');
+        e.target.classList.remove('green');
+        setIdx(idx+1);
+      }                            
+    }, 1000);
+  };
+  const { setGameOver, questions, count, setCount } = props;  
+  return (
+    <>
+      <h2>{count}/{questions.length}</h2>
+      <h1>{questions[idx].question}</h1>
+      {questions[idx].answers.map(a => <button className="choice" key={a} onClick={handleClick}>{a}</button>)}
+    </>
+  );
+};
+
+const GameOver = props => {
+  
+  const { restartGame, count } = props;
   
   return (
-    <h1>Ejercicio 29, dale animate, hacelo vos</h1>
+    <>
+      <h1>Le pegaste a {count} de {questions.length}</h1>
+      <h2>{count < 4 ? 'Sos malo' : count < 10 ? 'Sos masomenos' : 'Sos un genio'} </h2>
+      <button onClick={restartGame}>Volver a jugar</button>
+    </>
   );
+};
 
+const Start = props => {
+  const { setPlaying } = props;
+  return (
+    <>
+      <h1>Trivia</h1>
+      <button onClick={() => setPlaying(true)}>Jugar</button>
+    </>
+  );
 };
 
 export default Trivia;
---
+++ src/29-trivia/questions.js
@@ -0,0 +1,64 @@
+const questions = [
+  {
+    id: 1,
+    question: '2 + 2?',
+    answers: ['1','2','3','4'],
+    correct: '4'
+  },
+  {
+    id: 2,
+    question: '4 + 2?',
+    answers: ['13','5','6','4'],
+    correct: '6'
+  },
+  {
+    id: 3,
+    question: '100 - 36',
+    answers: ['32','64','100','36'],
+    correct: '64'
+  },
+  {
+    id: 4,
+    question: '25 / 5',
+    answers: ['50','25','5','1'],
+    correct: '5'
+  },
+  {
+    id: 5,
+    question: '5 * 5',
+    answers: ['50','125','100','40'],
+    correct: '125'
+  },
+  {
+    id: 6,
+    question: '1000 - 10',
+    answers: ['999','990','1000','1110'],
+    correct: '990'
+  },
+  {
+    id: 7,
+    question: '2^10',
+    answers: ['4096','256','512','1024'],
+    correct: '1024'
+  },
+  {
+    id: 8,
+    question: '8 * 8',
+    answers: ['125','49','55','64'],
+    correct: '64'
+  },
+  {
+    id: 9,
+    question: '4 * 3',
+    answers: ['6','18','12','13'],
+    correct: '12'
+  },
+  {
+    id: 10,
+    question: '1234 + 1234',
+    answers: ['2468','2466','1234','4321'],
+    correct: '2468'
+  },
+];
+
+export default questions;
